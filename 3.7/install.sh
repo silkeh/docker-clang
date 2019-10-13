04717887add392fbe8bfbc1a3b3f288fbfc7fdb4
Silke Hofstra <silke@slxh.eu>
2019-10-13 15:58:04 +0200

    Regenerate Dockerfiles

--- 3.7/install.sh
+++ 3.7/install.sh
@@ -74,6 +74,9 @@ case "${ARCH}:${LLVM_VERSION}" in
 "x86_64:8.0."*)
   PLATFORM="linux-gnu-ubuntu-18.04"
   ;;
+"x86_64:9.0."*)
+  PLATFORM="pc-linux-gnu"
+  ;;
 esac
 
 if [ -z ${DOWNLOAD_PLATFORM+x} ]; then
