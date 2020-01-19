8ab7969b0996ae937eff719e095f16e24723e8dc
Silke Hofstra <silke@slxh.eu>
2020-01-19 15:05:40 +0100

    Make docker image more configurable

--- generate.sh
+++ generate.sh
@@ -7,10 +7,14 @@ OLD_VERSIONS="3.3 3.4.2 3.5.2 3.6.2 3.7.1 3.8.1 3.9.1"
 # Versions using the new versioning scheme
 NEW_VERSIONS="4.0.1 5.0.2 6.0.1 7.0.1 8.0.0 9.0.0"
 
+# Default docker image
+DOCKER_IMAGE="debian:buster"
+
 # Create a Dockerfile from template
 _create() {
   v="$1"
   r="$2"
+  d="${DOCKER_IMAGE}"
 
   mkdir -p "${r}"
   cp -a "Dockerfile.template" "${r}/Dockerfile"
@@ -18,6 +22,7 @@ _create() {
 
   sed -i "s/{release}/$r/g" "${r}/Dockerfile"
   sed -i "s/{version}/$v/g" "${r}/Dockerfile"
+  sed -i "s/{image}/$d/g"  "${r}/Dockerfile"
 }
 
 # Get latest version
