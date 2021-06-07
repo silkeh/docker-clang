fe67817969dc76e5d61aabd915452f30101d4ccd
Bensuperpc <bensuperpc@gmail.com>
2021-06-07 21:13:09 +0200

    Update github CI and default image to bullseye
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ OLD_VERSIONS="3.3 3.4.2 3.5.2 3.6.2 3.7.1 3.8.1 3.9.1"
 NEW_VERSIONS="4.0.1 5.0.2 6.0.1 7.0.1 8.0.0 9.0.0 10.0.0 11.0.0 12.0.0"
 
 # Default docker image
-DOCKER_IMAGE="debian:buster"
+DOCKER_IMAGE="debian:bullseye"
 
 # Create a Dockerfile from template
 _create() {
